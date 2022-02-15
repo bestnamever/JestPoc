2a44eec0035e8df5cc21e0a3fa804f94557b027d
bestnamever <neverhasthisname@gmail.com>
2022-02-15 09:31:42 +0000

    Deploying to gh-pages from @ bestnamever/JestPoc@15e0ca8b23623132c08099d18d23c70b45573e99 🚀

---
+++ precache-manifest.7d7ff554237aa23d3a393d90830f4ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d93dcf9733bdcba579ed00e92c1b3a",
+    "url": "/JestPoc/index.html"
+  },
+  {
+    "revision": "405edf4c8a8c2dc8192c",
+    "url": "/JestPoc/static/css/main.77ee6f1b.chunk.css"
+  },
+  {
+    "revision": "5bb2684218b63c458b92",
+    "url": "/JestPoc/static/js/2.c0f4745d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JestPoc/static/js/2.c0f4745d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405edf4c8a8c2dc8192c",
+    "url": "/JestPoc/static/js/main.ecb93859.chunk.js"
+  },
+  {
+    "revision": "20bebd348231ab5f1e9b",
+    "url": "/JestPoc/static/js/runtime-main.995e13f8.js"
+  }
+]);
