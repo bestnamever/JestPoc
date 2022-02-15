5457c18ae5ca9b4ae2b186293a66a03c8f262741
bestnamever <neverhasthisname@gmail.com>
2022-02-15 09:21:54 +0000

    Deploying to gh-pages from @ bestnamever/JestPoc@fb8fa2163b21347f3421d41405d858e984aa9653 🚀

---
+++ precache-manifest.f94a22be06486eca9073a4e808a36a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ec67a9d2e161ef990636bd09709e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e893f57f2c29cd3e286",
+    "url": "/static/css/main.77ee6f1b.chunk.css"
+  },
+  {
+    "revision": "5bb2684218b63c458b92",
+    "url": "/static/js/2.c0f4745d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c0f4745d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e893f57f2c29cd3e286",
+    "url": "/static/js/main.5a17006c.chunk.js"
+  },
+  {
+    "revision": "9851f84acb931925f4ab",
+    "url": "/static/js/runtime-main.912cce5b.js"
+  }
+]);
