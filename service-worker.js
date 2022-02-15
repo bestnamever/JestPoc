d31bf0a2d018bce6f68d50ae37ebcd950c9770e1
bestnamever <neverhasthisname@gmail.com>
2022-02-15 10:25:01 +0000

    Deploying to gh-pages from @ bestnamever/JestPoc@5ee8fbac81eac3ab4d7a803ab201a75ca5d52ed5 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JestPoc/precache-manifest.7d7ff554237aa23d3a393d90830f4ff4.js"
+  "/precache-manifest.a5d3e7afef3650eef8e30528fa0b3dfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JestPoc/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
